e8b266f9ed4d042aec3eaadf887e51e6f374f42e
UmitBalikci <u.balikci.bm@gmail.com>
2024-03-14 14:58:32 +0300

    add charts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
 
+import * as Signalr from "@microsoft/signalr";
+
+import * as Highcharts from "highcharts";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ChartsClient';
+  Highcharts : typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    title: {
+      text : "Başlık"
+    },
+    subtitle :{
+      text : "Alt Başlık"
+    },
+    yAxis : {
+      title : {
+        text : "Y Ekseni"
+      }
+    },
+    xAxis : {
+      accessibility : {
+        rangeDescription: "2020-2024"
+      }
+    },
+    legend : {
+      layout : "vertical",
+      align : "right",
+      verticalAlign : "middle"
+    },
+    series : [
+      {
+        name : "X",
+        data : [1000, 2000, 3000],
+        type: "line"
+      },
+      {
+        name : "Y",
+        data : [5000, 3000, 2000],
+        type: "line"
+      },
+      {
+        name : "Z",
+        data : [815, 548, 1450],
+        type: "line"
+      }
+    ],
+    plotOptions : {
+      series: {
+        label : {
+          connectorAllowed : true
+        },
+        pointStart : 100
+      }
+    }
+  }
 }
